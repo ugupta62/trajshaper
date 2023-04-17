80260c3269e874e2ebf44835f8411fe903b06341
ugupta62 <ugupta62@gatech.edu>
2023-04-16 20:22:17 -0400

    added waypoint_navigator

--- src/config.py
+++ src/config.py
@@ -12,7 +12,7 @@ user_study_folder = "/home/arthur/local_data/user_study"
 base_folder = "/home/arthur/local_data/"
 data_folder = "/home/arthur/local_data/data/"
 # models_folder = "/home/arthur/data/models/"
-models_folder = "/home/arthur/local_data/models/"
+models_folder = "/home/catkin_ws/src/latte/models/"
 
 # image_dataset_folder = "/home/arthur/data/image_dataset/"
 image_dataset_folder = "/home/arthur/image_dataset/"
